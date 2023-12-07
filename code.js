45f8c84c4bab9d65f2bbc021f5373b3989ee8b28
Nethmi Silva <91644460+NethmiSilva@users.noreply.github.com>
2023-12-07 17:08:55 +0530

    Update code.js

--- code.js
+++ code.js
@@ -12,6 +12,23 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+});
 function closeNavigation() {
   var x = document.getElementById("myTopnav");
   if (x) {
